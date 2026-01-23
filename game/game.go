c96d5b3a36a75c070a0735ccd187a6ff8e8f2733
MishaAC <cristhianalbites233@gmail.com>
2026-01-22 19:11:26 -0500

    added game logic

--- game/game.go
+++ game/game.go
@@ -1,1 +1,49 @@
 package game
+
+import "math/rand"
+
+type Result int
+
+const (
+	TooLow Result = iota
+	TooHigh
+	Correct
+	GameOver
+)
+
+type Game struct {
+	secretNumber  int
+	min           int
+	max           int
+	attemptsLeft  int
+	totalAttempts int
+}
+
+func New(min, max, attempts int) *Game {
+	return &Game{
+		secretNumber:  rand.Intn(max-min+1) + 1,
+		min:           min,
+		max:           max,
+		attemptsLeft:  attempts,
+		totalAttempts: attempts,
+	}
+}
+
+func (g *Game) Guess(number int) Result {
+	if g.attemptsLeft <= 0 {
+		return GameOver
+	}
+	g.attemptsLeft--
+	switch {
+	case number < g.secretNumber:
+		return TooLow
+	case number > g.secretNumber:
+		return TooHigh
+	default:
+		return Correct
+	}
+}
+
+func (g *Game) AttemptsLeft() int {
+	return g.attemptsLeft
+}
